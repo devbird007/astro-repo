0a096cd8a47c80f0878eea62e07f33a0888bc049
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-12-09 08:06:46 -0600

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import awsAmplify from 'astro-aws-amplify';
- 
-import sitemap from '@astrojs/sitemap';
- 
+
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  adapter: awsAmplify(),
-  output: 'server',
-});
+export default defineConfig({});
